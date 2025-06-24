1c3aa6410cd4d8f55cdefbb88bbfcdd21b268a67
wasifkabir <wasifulkabir9016@gmail.com>
2025-06-24 12:04:46 +0800

    Add functionality to add todo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import { todos, printTodos } from "./todo";
+import { todos, addTodo } from "./todo";
 import "./styles.css";
 
-console.log(todos);
-printTodos();
+const newTodoBtn = document.querySelector("#new-button");
+const formContainer = document.querySelector("#todo-form-container");
+const todoName = document.querySelector("#todo-name")
+const todoDesc = document.querySelector("#todo-description")
+const form = document.querySelector("#todo-form")
+const addBtn = document.querySelector("#add-button")
+const todoList = document.querySelector("#todo-list")
+
+newTodoBtn.addEventListener("click", () => {
+  formContainer.style.display = "block";
+  todoName.focus();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = todoName.value.trim();
+  if (!name) return;
+  const desc = todoDesc.value.trim();
+  addTodo(name, desc);
+
+  form.reset();
+  formContainer.style.display = "none";
+
+  todoList.innerHTML = "";
+  todos.forEach(todo => {
+    const li = document.createElement("li");
+    li.textContent = name;
+    todoList.appendChild(li);
+  });
+});
--- src/todo.js
+++ src/todo.js
@@ -13,20 +13,13 @@ function createTodo(name, description = "", dueDate = null, priority = 4) {
 function addTodo(name, desc, dueDate, priority){
   const todo = createTodo(name, desc, dueDate, priority)
   todos.push(todo)
-}
-
-function sortTodo() {
   todos.sort((a,b) => a.priority - b.priority);
 }
 
 function printTodos() {
-  sortTodo();
   todos.forEach(todo => {
     console.log(todo);
   });
 }
 
-addTodo("sdsf");
-addTodo("css");
-
-export {todos, printTodos};
+export {todos, addTodo};
