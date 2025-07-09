8c9136d42224dbdedadf36b624ac51a6ae48abfa
mdwasifulkabir <wasifulkabir9016@gmail.com>
2025-07-09 22:24:45 +0800

    Add project input dialog

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { todos, AddTodo } from "./todo";
+import { todos, AddTodo, projects, AddProject } from "./todo";
 import { RenderTodos } from "./dom.js";
 import "./styles.css";
 
@@ -10,6 +10,11 @@ const todoDueDate = document.querySelector("#todo-date");
 const form = document.querySelector("#todo-form");
 const addBtn = document.querySelector("#add-button");
 const cancelBtn = document.querySelector("#cancel-button");
+const addProjectBtn = document.querySelector("#add-project-button");
+const projectInputCancelBtn = document.querySelector("#project-input-cancel-button")
+const projectName = document.querySelector(".project-name-input");
+const projectDialog = document.querySelector(".project-dialog");
+const projectForm = document.querySelector(".project-form")
 const todoList = document.querySelector("#todo-list");
 
 newTodoBtn.addEventListener("click", () => {
@@ -20,6 +25,14 @@ newTodoBtn.addEventListener("click", () => {
 cancelBtn.addEventListener("click", () => {
   formContainer.style.display = "none";
 })
+
+addProjectBtn.addEventListener("click", () => {
+  projectDialog.showModal();
+});
+
+projectInputCancelBtn.addEventListener("click", () => {
+  projectDialog.close();
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -33,4 +46,14 @@ form.addEventListener("submit", (e) => {
   formContainer.style.display = "none";
 
   RenderTodos();
+});
+
+projectForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = projectName.value.trim();
+  if (!name) return;
+  AddProject(name);
+
+  projectForm.reset();
+  projectDialog.close();
 });
--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,7 @@
 const newBtn = document.querySelector("#new-button");
+const newProjectBtn = document.querySelector("#add-project-button")
 let todos = []
+let projects = []
 
 function createTodo(name, description = "", dueDate = null, priority = 4) {
   return {
@@ -10,9 +12,21 @@ function createTodo(name, description = "", dueDate = null, priority = 4) {
   };
 }
 
+function createProject(name) {
+  return {
+    name,
+    todos : [],
+  };
+}
+
 function AddTodo(name, desc, dueDate, priority){
   const todo = createTodo(name, desc, dueDate, priority)
   todos.push(todo)
 }
 
-export {todos, AddTodo};
+function AddProject(name) {
+  const project = createProject(name); 
+  projects.push(project);
+}
+
+export {todos, AddTodo, projects, AddProject};
